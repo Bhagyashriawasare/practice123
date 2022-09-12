0f5fd66ee3dd64926835ad125beaeb7c51400cc4
Bhagyashri <Bhagyashri@SONY-LAPTOP>
2022-09-12 20:35:20 +0530

    hii

---
+++ src/main/java/Practice/test0.java
@@ -0,0 +1,13 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class test0 {
+	@Test
+	public void tc0() 
+	{
+		System.out.println("selenium with java");
+	}
+	
+
+}
---
+++ src/main/java/Practice/test1.java
@@ -0,0 +1,12 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class test1 {
+	@Test
+	public void tc1() 
+	{
+		System.out.println("selenium with python");
+	}
+
+}
